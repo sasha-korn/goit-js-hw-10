698de216152a208e5f0588978e7ef5792bd8bb5b
sasha-korn <korniichukaleksandra@gmail.com>
2024-08-26 21:58:06 +0100

    setup

---
+++ src/js/1-timer.js
@@ -0,0 +1,5 @@
+import flatpickr from 'flatpickr';
+// Described in the documentation
+import iziToast from 'izitoast';
+// Additional import of styles
+import 'izitoast/dist/css/iziToast.min.css';
---
+++ src/js/2-snackbar.js
@@ -0,0 +1,4 @@
+// Described in the documentation
+import iziToast from 'izitoast';
+// Additional import of styles
+import 'izitoast/dist/css/iziToast.min.css';

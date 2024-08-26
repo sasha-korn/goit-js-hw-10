7abc5c48bd5777937969b82dbff426cb35582d75
sasha-korn <korniichukaleksandra@gmail.com>
2024-08-26 23:45:19 +0100

    task 2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,3 +2,46 @@
 import iziToast from 'izitoast';
 // Additional import of styles
 import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  const delay = document.querySelector('[name="delay"]').value.trim();
+  const isFulfilled =
+    document.querySelector('[name="state"]:checked').value === 'fulfilled';
+  //   const isRejected = document.querySelector('[value="rejected"]:checked').value;
+  console.log(delay, isFulfilled);
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (isFulfilled) {
+        resolve(`✅ Fulfilled promise in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise in ${delay}ms`);
+      }
+    }, delay);
+  })
+    .then(message => {
+      showNotification(message, 'limegreen');
+    })
+    .catch(err => {
+      showNotification(err, 'indianred');
+    });
+
+  form.reset();
+}
+
+function showNotification(message, color) {
+  iziToast.show({
+    message: message,
+    position: 'topRight',
+    // color: color,
+    progressBar: false,
+    theme: 'dark',
+    backgroundColor: color,
+  });
+}
+
+// https://marcelodolza.github.io/iziToast/#Start
